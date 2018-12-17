21937e9c5b6ce89d15f4b5fc5c0aabd107b0079a
santro85 <45917354+santro85@users.noreply.github.com>
2018-12-17 10:42:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,75 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
+// // Set constraints for the video stream
+// var constraints = { video: { facingMode: "user" }, audio: false };
+// // Define constants
+// const cameraView = document.querySelector("#camera--view"),
+//     cameraOutput = document.querySelector("#camera--output"),
+//     cameraSensor = document.querySelector("#camera--sensor"),
+//     cameraTrigger = document.querySelector("#camera--trigger")
+// // Access the device camera and stream to cameraView
+// function cameraStart() {
+//     navigator.mediaDevices
+//         .getUserMedia(constraints)
+//         .then(function(stream) {
+//         track = stream.getTracks()[0];
+//         cameraView.srcObject = stream;
+//     })
+//     .catch(function(error) {
+//         console.error("Oops. Something is broken.", error);
+//     });
+// }
+// // Take a picture when cameraTrigger is tapped
+// cameraTrigger.onclick = function() {
+//     cameraSensor.width = cameraView.videoWidth;
+//     cameraSensor.height = cameraView.videoHeight;
+//     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+//     cameraOutput.src = cameraSensor.toDataURL("image/webp");
+//     cameraOutput.classList.add("taken");
+// };
+// // Start the video stream when the window loads
+// window.addEventListener("load", cameraStart, false);
+
+
+
+
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
     });
 }
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+
+/* Legacy code below: getUserMedia
+else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.srcObject = stream;
+        video.play();
+    }, errBack);
+}
+*/
+
+
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
